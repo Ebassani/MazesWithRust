cfc0184367de544ffe80d05329a83caf99836ef8
Edu <eduardo.sbassani@gmail.com>
2023-03-02 16:09:52 +0200

    Checks for all available neighbors of the cell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 fn main() {
     let maze_height = 10;
     let maze_width = 10;
@@ -20,39 +22,70 @@ fn main() {
 
 
     //Closure that return the place in the vector of a coordenate
-    let coordenate = |x: u32, y: u32,north:bool, east: bool| -> u64  {
-        let mut calc_y =  current.last().unwrap().1;
+    let coordenate = |x: u32, y: u32,north:bool, east: bool| -> usize  {
         if north {
-            calc_y -= y;
+            let calc = (current.last().unwrap().1) - y * maze_width + (current.last().unwrap().0 + x);
+            let calc = usize::try_from(calc).unwrap();
+            return calc
         }
-        else {
-            calc_y += y;
-        }
-
-        let mut calc_x = current.last().unwrap().0;
         if east {
-            calc_x -= x;
-        }
-        else {
-            calc_x += x;
+            let calc = (current.last().unwrap().1) + y * maze_width + (current.last().unwrap().0 - x);
+            let calc = usize::try_from(calc).unwrap();
+            return calc
         }
 
-        let calc:u64 = (calc_y * maze_width + calc_x).into();
+        let calc = (current.last().unwrap().1) + y * maze_width + (current.last().unwrap().0 + x);
+        let calc = usize::try_from(calc).unwrap();
         calc
     };
 
     if visited < maze_height * maze_width{
-        //Checks if it is possible for there to be a cell North of the current one
-        if current.last().unwrap().1 > 0{
-            //If there is a cell a above the current one, check if it is visited
-            if cells[usize::try_from(coordenate(0,1,true,false)).unwrap()].2 {
-                print!("{}{}",cells[usize::try_from(coordenate(1,0,true,false)).unwrap()].0,cells[usize::try_from(coordenate(1,0,true,false)).unwrap()].1);
-                print!("free")
-            }
-            else {
-                print!("occupied")
+
+        let mut free_cells: Vec<u32> = Vec::new();
+
+        //Checks if it is possible for there to be a cell in a certain direction
+        //If there is a cell there, check if it is visited
+        //In case the cell is free, save it in a vector
+        if current.last().unwrap().1 > 0 && cells[coordenate(0,1,true,false)].2 {
+            free_cells.push(1);
+        }
+        if current.last().unwrap().1 < maze_height && cells[coordenate(0,1,false,false)].2 {
+            free_cells.push(2);
+        }
+        if current.last().unwrap().0 > 0 && cells[coordenate(1,0,false,true)].2 {
+            free_cells.push(3);
+        }
+        if current.last().unwrap().0 < maze_width && cells[coordenate(1,1,false,false)].2 {
+            free_cells.push(4);
+        }
+
+        if free_cells.len() > 0 {
+            let mut rng = rand::thread_rng();
+            let chosen_cell = free_cells[rng.gen_range(0..free_cells.len())];
+
+            match chosen_cell {
+                1 => {
+                    let position = coordenate(0,1,true,false);
+                    current.push((cells[position].0,cells[position].1));
+                    
+                },
+                2 => {
+                    let position = coordenate(0,1,true,false);
+                    current.push((cells[position].0,cells[position].1));
+                },
+                3 => {
+                    let position = coordenate(0,1,true,false);
+                    current.push((cells[position].0,cells[position].1));
+                },
+                4 => {
+                    let position = coordenate(0,1,true,false);
+                    current.push((cells[position].0,cells[position].1));
+                }
+                _ => print!("error")
             }
         }
+
+        
     }
     
     
