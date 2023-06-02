cefd2efec444c148215977c7030274f9814140b9
ebassani <eduardo.sbassani@gmail.com>
2023-06-01 23:40:35 -0300

    Fixing wrong north and south linking on generate

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,28 @@ mod cell_struct;
 pub mod generate;
 use generate::maze;
 
-pub mod solve;
-
 fn main() {
 
     let maze_height = 10;
     let maze_width = 10;
 
     let maze = maze(maze_height, maze_width);
-    
 
-    for i in 0..maze_height{
+    /* for cell in maze {
+        print!("C({},{})", cell.get_y(), cell.get_x());
+        print!("N{},S{},L{},O{}\n", cell.get_n(),cell.get_s(),cell.get_e(),cell.get_w() )
+    } */
+    
+    for i in (0..maze_height).rev() {
         for j in 0..maze_width {
-            if maze[(i*10 + j) as usize].get_w() {
+            if maze[(i*maze_height + j) as usize].get_w() {
                 print!("-");
             }
             else {
                 print!("|");
             }
             print!("c");
-            if maze[(i*10 + j) as usize].get_e() {
+            if maze[(i*maze_height + j) as usize].get_e() {
                 print!("-");
             }
             else {
@@ -31,7 +33,7 @@ fn main() {
         print!("\n");
         for j in 0..maze_width {
             print!(" ");
-            if maze[(i*10 + j) as usize].get_s() {
+            if maze[(i*maze_height + j) as usize].get_s() {
                 print!("|");
             }
             else {
